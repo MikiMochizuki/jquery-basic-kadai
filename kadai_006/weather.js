a2577292fd241b9fd40b7f794530f823b5edac2d
モチヅキミキ <tonton0312tonton@gmail.com>
2024-09-05 23:37:22 +0900

    kadai_006を修正

--- kadai_006/weather.js
+++ kadai_006/weather.js
@@ -1,7 +1,4 @@
 $(function () {
-  // id属性がaddの要素がクリックされたら
-  $("button").on("click", function () {
-    // id属性がtargetの要素にclass属性'heading'を追加
-    $("#target").addClass("heading");
-  });
+  // id属性がtargetの要素にclass属性'heading'を追加
+  $("#target").addClass("heading");
 });
